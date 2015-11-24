b8f9e11b3a28b90f1c677707159aecd4a1aeecca
fg <adamwu2144@gmail.com>
2015-11-24 14:45:09 +0800

    Initial Commit

---
+++ MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by fg on 2015/11/24.
+//  Copyright © 2015年 fg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
